35a7424f8f31a53d6fa138206f9ab25fdf9bbbfc
Alketa Memaj <memajalketa92@gmail.comgit config --global user.email memajalketa92@gmail.comgit config --global user.name Alketa>
2024-07-25 11:50:34 +0200

    displayBook function create a card every time a user add a new book
     The card will contain the information added by user and the function displayBook will be called every time the user click sumbit with the book data.

--- library.js
+++ library.js
@@ -30,12 +30,29 @@ function addBookToLibrary() {
     //store the values to the constructor
     let BookAdded = new Book(author, title, numOfPages, read);
     myLibrary.push(BookAdded);
-    console.log(myLibrary);
+    displayBook();
 }
+
+function displayBook() {
+    let bookDisplayDiv = document.querySelector("#book_display");
+    bookDisplayDiv.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book_i = myLibrary[i];
+        let book_i_card = document.createElement("div");
+        book_i_card.id = "book_card";
+        book_i_card.innerHTML = `
+         <h2> Author: ${book_i.author} </h2>
+         <h3> Title: ${book_i.title} </h3>
+         <h4> Pages: ${book_i.numOfPages} </h4>
+         <h5> ${book_i.read ? "Read" : "Not Read"} </h5>`
+
+        bookDisplayDiv.appendChild(book_i_card);     
+    }
+}
+
 
 addBookForm.addEventListener("submit", function () {
     // call the function every time the user click submit
     event.preventDefault();
     addBookToLibrary();
 });
-
